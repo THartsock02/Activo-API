918062bf764eef832e31fdb18c3aeba9cc3ef769
Tyler Hartsock <thartsock02@gmail.com>
2023-07-29 12:29:15 -0500

    Helmet, eslint and prettier

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express from 'express'
+import helmet from 'helmet'
+import dotenv from 'dotenv'
+import routes from './src/routes'
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = process.env.PORT;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server test test');
-});
+app.use(helmet())
+app.use(routes)
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
